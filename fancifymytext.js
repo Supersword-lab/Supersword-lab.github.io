bbedd40885527c4c7b5329179a6406ad5586122b
Supersword-lab <nitinkowdle@gmail.com>
2025-12-05 00:51:47 -0800

    Update fancifymytext.js

--- fancifymytext.js
+++ fancifymytext.js
@@ -5,19 +5,19 @@ function big() {
 function changeStyle(){
     alert("Change style")
     if (document.getElementById("fancy").checked){
-        document.getElementById("textBox").style.fontWeight = 'bold';
-        document.getElementById("textBox").style.color = 'blue';
-        document.getElementById("textBox").style.textDecoration = 'underline';
+        document.getElementById("textBox").style.fontWeight = "bold";
+        document.getElementById("textBox").style.color = "blue";
+        document.getElementById("textBox").style.textDecoration = "underline";
     }
     else if (document.getElementById("boring").checked){
-        document.getElementById("textBox").style.fontWeight = 'normal';
-        document.getElementById("textBox").style.color = 'black';
-        document.getElementById("textBox").style.textDecoration = 'none';
-        document.getElementById("textBox").style.textTransform = 'none'
+        document.getElementById("textBox").style.fontWeight = "normal";
+        document.getElementById("textBox").style.color = "black";
+        document.getElementById("textBox").style.textDecoration = "none";
+        document.getElementById("textBox").style.textTransform = "none";
     }
 }
 function moo(){
-    document.getElementById("textBox").style.textTransform = 'uppercase'
+    document.getElementById("textBox").style.textTransform = "uppercase";
     let text = document.getElementById("textBox").value;
     let parts = text.split(".");
     text = parts.join("-Moo");
